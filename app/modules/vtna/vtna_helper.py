fixed TC normalization; added poisoning, cleaned up functions

app/modules/vtna/vtna_helper.py
@@ -1,6 +1,8 @@
 """pre plotting data manipulation"""
 import pandas as pd
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 def load_raw(filename):
     xl = pd.ExcelFile(filename)
@@ -8,43 +10,38 @@ def load_raw(filename):
     # import excel sheets of reaction 1 and 2
     for i in range(len(xl.sheet_names)):
         raw_data.append(pd.read_excel(filename, i))
-        #print(f"\ncolumns read from experiment {i+1}: \n{len(raw_data[i].columns)}")
     return raw_data, xl.sheet_names
 
 #produce a column summing all counts at each
 # timestep for total ion count normalization
-def get_TC(df):
-    totaled = np.zeros(len(df))
-    for j,col in enumerate(df.columns):
-        if j > 0:
-            totaled += df[col]
-    return totaled
+def get_TC(data):
+    totals = []
+    for df in data:
+        totals.append(df.iloc[:, 1:].sum(axis=1))
+    return totals
 
 def testtc(df):
     return df.values[:, 1:].sum(axis=1)
 
-def get_sheet_totals(normalization_method, raw_data):
+def get_sheet_totals(normalization_method, data):
     """returns chosen normalization value"""
     totals = []
     if normalization_method == "TC":
-        for df in raw_data:
-            totals.append(get_TC(df))
+        totals = get_TC(data)
     elif normalization_method == "MV":
-        for df in raw_data:
+        for df in data:
             totals.append(df.iloc[:, 1:].max().max())
     # if neither TC nor MV is selected, the operations of Total1 and Total2
     # will not change any values
     else:
-        totals = [1]*len(raw_data)
+        totals = [1]*len(data)
     return totals
 
-def normalize_columns(raw_data, totals):
+def normalize_columns(data, totals):
     """function that normalizes all columns by the sum on that time step (excludes the time coumn in a sheet)"""
     Rnorm = []
-    for i, df in enumerate(raw_data):
-        sheetnorm = df.copy()
-        sheetnorm.iloc[:, 1:] /= totals[i]
-        Rnorm.append(sheetnorm)
+    for i, df in enumerate(data):
+        Rnorm.append(pd.concat([df.iloc[:,0], df.iloc[:, 1:].div(totals[i], axis=0)], axis=1))
     return Rnorm
 
 def get_max_times(Rnorm):
@@ -63,15 +60,19 @@ def select_data(Rnorm, reactions=None, species=None):
         return [Rnorm[rxn] for rxn in reactions]
     return [Rnorm[rxn].iloc[:, [0]+[spec+1 for spec in species]] for rxn in reactions]
 
+
 if __name__ == "__main__":
     filename = "sampledata/VTNA329.xlsx"
     raw_data, sheet_names = load_raw(filename)
-    totals = get_sheet_totals('TC', raw_data)
-    norm_data = normalize_columns(raw_data, totals)
-    print(norm_data[0].head())
-    max = get_max_times(norm_data)
-    print(max)
-    print(sheet_names)
-    gimme = select_data(norm_data, [0,1], [0, 1])
-    print(gimme[1].head())
-    print(f'{len(gimme[0].columns)}')
+    totals = get_TC(raw_data)
+    for total in totals:
+        print(total.head())
+    # totals = get_sheet_totals('TC', raw_data)
+    # norm_data = normalize_columns(raw_data, totals)
+    # print(norm_data[0].head())
+    # max_times = get_max_times(norm_data)
+    # print(max_times)
+    # print(sheet_names)
+    # gimme = select_data(norm_data, [0,1], [0, 1])
+    # print(gimme[1].head())
+    # print(f'{len(gimme[0].columns)}')